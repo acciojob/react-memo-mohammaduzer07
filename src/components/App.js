b6e4f698c3a49cd2c87a1ed9a335d6630edd32c0
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-06-27 16:43:00 +0530

    submission 2

--- src/components/App.js
+++ src/components/App.js
@@ -18,15 +18,15 @@ const App = () =>{
         <div id="main">
             <h1>My todos</h1>
             <p id="todo-0">{newTodo}</p>
-            <button onClick={() => setNewTodo('New Todo')}>Add Todo</button>
+            <button id="add-todo-btn" onClick={() => setNewTodo('New Todo')}>Add Todo</button>
             <div id="incr-cnt">
                 Count: 0 <button onClick={() => setCount(count + 1)}>+</button>
-                <div>{count}</div>
+                <div id="calc">{count}</div>
             </div>
 
             <h1>React.memo</h1>
 
-            <input value={input} type="text" placeholder="todo task" onChange={(e) => setInput(e.target.value)} />    
+            <input id="skill-input" value={input} type="text" placeholder="todo task" onChange={(e) => setInput(e.target.value)} />    
             <button id="skill-btn" onClick={() => setTodo([...todo, input])}>Add Skill</button>
 
             <ul id="item-jumbotron">
